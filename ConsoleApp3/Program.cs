ea396208cb9f4cba86d4ce7d9eeeb7def86a3596
Daan Le Duc <daanleduc@outlook.com>
2019-08-30 08:35:46 +0200

    Trying to create shardCollection

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Events;
 
 namespace ConsoleApp3
 {
@@ -19,8 +22,29 @@ namespace ConsoleApp3
 
             var connectionString = $@"mongodb://{username}:{password}@{username}.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&appName=@{username}@";
 
+            var blacklistedCommands = new[]
+            {
+                "isMaster",
+                "buildInfo",
+                "saslContinue",
+                "saslStart",
+                "getLastError",
+            };
+
             var clientSettings = MongoClientSettings.FromConnectionString(connectionString);
 
+            clientSettings.ClusterConfigurator = cb =>
+            {
+                cb.Subscribe<CommandStartedEvent>(e =>
+                {
+                    if (blacklistedCommands.Contains(e.CommandName))
+                    {
+                        return;
+                    }
+
+                    Console.WriteLine($"{e.CommandName} -> {e.Command}");
+                });
+            };
             clientSettings.SslSettings = new SslSettings
             {
                 CheckCertificateRevocation = false
@@ -31,7 +55,26 @@ namespace ConsoleApp3
 
             var database = mongoClient.GetDatabase(databaseName);
 
-            database.CreateCollection("test");
+            var collectionName = "test";
+            var shardKey = "shard";
+
+            var partition = new BsonDocument {
+                {
+                    "shardCollection", $"{database.DatabaseNamespace}.{collectionName}"
+                },
+                {
+                    "key", new BsonDocument
+                    {
+                        {
+                            shardKey, "hashed"
+                        }
+                    }
+                }
+            };
+
+            var command = new BsonDocumentCommand<BsonDocument>(partition);
+
+            database.RunCommand(command);
 
             Console.WriteLine("Done");
         }
